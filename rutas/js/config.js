9c37d1ab29dbf56677bd0df230c2f2dfa4537927
mounir <chermounir@hotmail.com>
2019-10-09 13:54:18 +0200

    rutas

---
+++ rutas/js/config.js
@@ -0,0 +1,24 @@
+
+app.config( function( $routeProvider ){
+
+  $routeProvider
+    .when('/',{
+        templateUrl: 'parciales/home.html'
+    })
+    .when('/componente1',{
+        templateUrl: 'parciales/componente.html'
+    })
+    .when('/componente2',{
+      template: '<componente-boton></componente-boton>'
+    })
+    .when('/profesores',{
+        templateUrl: 'parciales/profesores.html'
+    })
+    .when('/creditos',{
+      templateUrl: 'parciales/creditos.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
+
+})
